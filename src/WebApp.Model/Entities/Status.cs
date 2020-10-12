65193e8c100e53e353c8d7b0bcad852170bca2e3
Umay Eras <umayeras@hotmail.com>
2020-10-12 13:20:45 +0300

    Entities added

---
+++ src/WebApp.Model/Entities/Status.cs
@@ -0,0 +1,15 @@
+namespace WebApp.Model.Entities
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    public enum StatusType
+    {
+        Active = 1,
+        Passive = 2,
+        Deleted = 3
+    }
+}
