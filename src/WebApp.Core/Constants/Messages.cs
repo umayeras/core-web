49afe1b3682bf31510e8944d5cc856218b6a8fc0
Umay Eras <umayeras@hotmail.com>
2020-10-13 12:54:42 +0300

    Messages class added

---
+++ src/WebApp.Core/Constants/Messages.cs
@@ -0,0 +1,8 @@
+namespace WebApp.Core.Constants
+{
+    public static class Messages
+    {
+        public static string GeneralError => "Something went wrong. Please try again later.";
+        public static string GeneralSuccess => "Operation is successful";
+    }
+}
